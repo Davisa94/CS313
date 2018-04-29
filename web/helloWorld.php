b6786cb32e606d7f97576978b08dc8666350babc
Austin Benitez <austin.i.davis94@gmail.com>
2018-04-28 21:34:05 -0600

    Added SOme Styling And hopefully Fancy Buttons

--- web/helloWorld.php
+++ web/helloWorld.php
@@ -1,10 +1,13 @@
 <?php
 echo "<!DOCTYPE html>
 <html lang = \"en\">
+<head>
 <meta charset = \"utf-8\" />
-<title>Week 11 : Problem 1</title>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\"/>
+<title>Hello World PHP</title>
+</head>
 <body>
-   <p> Hello World</p>
+   <p id=\"main\"> Hello World</p>
    <a href=\"HelloWorld.html\">Back to HTML version</a>
 </body></html>";
 ?>
--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 echo "<!DOCTYPE html>
 <html lang = \"en\">
+<head>
 <meta charset = \"utf-8\" />
-<title>Week 11 : Problem 1</title>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\"/>
+<title>Homepage</title>
+</head>
+
 <body>
    <p> Hello World</p>
    <a href=\"HelloWorld.html\">Hello World Assignment one</a>
