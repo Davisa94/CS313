bae26131780f6d75b6b1b0279a476d2c98e532d0
Austin Benitez <austin.i.davis94@gmail.com>
2018-04-28 21:08:36 -0600

    Added an index page that goes to the helloWorld

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<!DOCTYPE html>
+<html lang = \"en\">
+<meta charset = \"utf-8\" />
+<title>Week 11 : Problem 1</title>
+<body>
+   <p> Hello World</p>
+   <a href=\"HelloWorld.html\">Hello World version</a>
+</body></html>";
+?>
