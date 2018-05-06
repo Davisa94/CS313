9ba3b91d3c65f0caee26ed52a95114915ccf9d52
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 17:16:40 -0600

    Checking if statment in homebutton

--- web/2ndWeek/homeButton.php
+++ web/2ndWeek/homeButton.php
@@ -1,10 +1,10 @@
 <?php
 $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-// $active = "";
-// if(strpos($url, 'home') !== false)
-// {
-//    $active = "true";
-// }
+$active = "";
+if(strpos($url, 'home') !== false)
+{
+   $active = "true";
+}
 
    echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../2ndWeek/home.php\">Home</a>";
 
