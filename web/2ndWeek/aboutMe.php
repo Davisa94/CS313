10a7fdb14e300fa41cde6dad7a982872b1e78325
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 16:22:18 -0600

    Added some fancy bg styling... hopefully

--- web/2ndWeek/aboutMe.php
+++ web/2ndWeek/aboutMe.php
@@ -6,10 +6,29 @@
          <?php require 'nav.php'; ?>
          <main class="mdl-layout__content" id="other">
             <!-- This div is for the SBS content -->
-            <h3>My interests and Hobbies</h3>
+            <h3>A Little About Me</h3>
             <div class="grid">
-               <ul class="mdl-list nested">
+               <ul class="mdl-list nested gold-gradient">
                   <li class="mdl-list__item">
+                     <h3>My Gaming Preferences</h3>
+                     <span class="mdl-list__item-primary-content">
+                        I love to play minecraft and terraria, and lots of other games. Mainly games that are
+                        open-world or sandbox or some combination of both
+                     </span>
+                  </li>
+               </ul>
+               <ul class="mdl-list nested bronze-gradient">
+                  <li class="mdl-list__item">
+                     <h3>My interests and Hobbies</h3>
+                     <span class="mdl-list__item-primary-content">
+                        I love to play minecraft and terraria, and lots of other games. Mainly games that are
+                        open-world or sandbox or some combination of both
+                     </span>
+                  </li>
+               </ul>
+               <ul class="mdl-list nested steel">
+                  <li class="mdl-list__item">
+                     <h3>My Career Aspirations</h3>
                      <span class="mdl-list__item-primary-content">
                         I love to play minecraft and terraria, and lots of other games. Mainly games that are
                         open-world or sandbox or some combination of both
