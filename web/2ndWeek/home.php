910f6e17e2cd1671342c65c871c30a3a52e0c214
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 15:30:43 -0600

    Added a nav.php and a head.php and require statments

--- web/2ndWeek/home.php
+++ web/2ndWeek/home.php
@@ -1,37 +1,14 @@
 <html>
-<head>
-   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-   <link rel="stylesheet" href="homeStyle.css">
-   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-</head>
+<?php
+   require 'head.php';
+ ?>
 
 <body>
 
       <div class="mdl-layout mdl-js-layout">
-           <header class="mdl-layout__header">
-             <div class="mdl-layout-icon"></div>
-             <div class="mdl-layout__header-row">
-               <span class="mdl-layout__title">Austin Benitez' Homepage</span>
-               <div class="mdl-layout-spacer"></div>
-               <nav class="mdl-navigation">
-                  <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href="/web/2ndWeek/home.php">
-                   Home
-                 </button>
-                 <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href="#">World.</a>
-                 <a class="mdl-navigation__link" href="#">How</a>
-                 <a class="mdl-navigation__link" href="#">Are</a>
-                 <a class="mdl-navigation__link" href="#">You?</a>
-               </nav>
-             </div>
-           </header>
-  <div class="mdl-layout__drawer">
-    <span class="mdl-layout__title">Assignments</span>
-    <nav class="mdl-navigation">
-      <a class="mdl-navigation__link" href="../HelloWorld.html">Week 01</a>
-      <a class="mdl-navigation__link" href="../2ndWeek/home.php">Week 02</a>
-    </nav>
-  </div>
+<?php
+   require 'nav.php';
+ ?>
   <main class="mdl-layout__content" id="main">
     <div>
       <header>
