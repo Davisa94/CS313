85a7b9ed4cdf95a6c2680d1a93c5f53eb6e81b9f
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 17:12:17 -0600

    added some semicolons

--- web/2ndWeek/homeButton.php
+++ web/2ndWeek/homeButton.php
@@ -1,9 +1,9 @@
 <?php
 $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-$active = ""
+$active = "";
 if(strpos($url, 'home') !== false)
 {
-   $active = "true"
+   $active = "true";
 }
 else {
    echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../2ndWeek/home.php\">Home</a>";
