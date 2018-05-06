e43182217dc636938aa817af26ddc04848816ea8
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 16:37:55 -0600

    added some more classes to css

--- web/2ndWeek/aboutMe.php
+++ web/2ndWeek/aboutMe.php
@@ -11,9 +11,9 @@
                <div>
                   <h3>My Gaming Preferences</h3>
                   <ul class="mdl-list nested gold-gradient">
-                     <li class="mdl-list__item content">
+                     <li class="mdl-list__item">
 
-                        <span class="mdl-list__item-primary-content">
+                        <span class="mdl-list__item-primary-content content">
                            I love to play minecraft and terraria, and lots of other games. Mainly games that are
                            open-world or sandbox or some combination of both. I also enjoy the Legend of Zelda
                            Series of games, my favorties being Wind Waker, OOT, and Majoras Mask
@@ -26,7 +26,7 @@
                   <ul class="mdl-list nested bronze-gradient">
                      <li class="mdl-list__item content">
 
-                        <span class="mdl-list__item-primary-content">
+                        <span class="mdl-list__item-primary-content content">
                            I love to play minecraft and terraria, and lots of other games. Mainly games that are
                            open-world or sandbox or some combination of both
                         </span>
@@ -37,7 +37,7 @@
                   <h3>My Career Aspirations</h3>
                   <ul class="mdl-list nested steel-gradient">
                      <li class="mdl-list__item content">
-                        <span class="mdl-list__item-primary-content">
+                        <span class="mdl-list__item-primary-content content">
                            I love to play minecraft and terraria, and lots of other games. Mainly games that are
                            open-world or sandbox or some combination of both
                         </span>
