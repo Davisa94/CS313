2e8aada1f45ad1ee04575d458acae7ec84860c29
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 15:44:43 -0600

    Added a lot of new style and created assignments page

--- web/2ndWeek/assignments.php
+++ web/2ndWeek/assignments.php
@@ -1,1 +1,24 @@
-require "nav.php"
+<html>
+   <<?php require 'head.php'; ?>
+
+   <body>
+      <div class="mdl-layout mdl-js-layout">
+         <?php require 'nav.php'; ?>
+         <main class="mdl-layout__content" id="main">
+            <ul class="mdl-list">
+               <li class="mdl-list__item">
+                  <a class="mdl-list__item-primary-content mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href=>
+                     Week 01: Hello World
+                  </a>
+               </li>
+               <li class="mdl-list__item">
+                  <a class="mdl-list__item-primary-content mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href=>
+                     Week 02: Homepage
+                  </a>
+               </li>
+            </ul>
+         </main>
+      </div>
+   </body>
+
+</html>
