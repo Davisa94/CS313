59e3b8deff070cb9e27eb436c5e07a913847ea21
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 15:21:26 -0600

    Added a test button

--- web/2ndWeek/home.php
+++ web/2ndWeek/home.php
@@ -9,24 +9,25 @@
 <body>
 
       <div class="mdl-layout mdl-js-layout">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout-icon"></div>
-    <div class="mdl-layout__header-row">
-      <span class="mdl-layout__title" style="text-align:center;">Austin Benitez' Homepage</span>
-      <div class="mdl-layout-spacer"></div>
-      <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="#">Hello</a>
-        <a class="mdl-navigation__link" href="#">World.</a>
-        <a class="mdl-navigation__link" href="#">How</a>
-        <a class="mdl-navigation__link" href="#">Are</a>
-        <a class="mdl-navigation__link" href="#">You?</a>
-      </nav>
-    </div>
-  </header>
+           <header class="mdl-layout__header">
+             <div class="mdl-layout-icon"></div>
+             <div class="mdl-layout__header-row">
+               <span class="mdl-layout__title">Austin Benitez' Homepage</span>
+               <div class="mdl-layout-spacer"></div>
+               <nav class="mdl-navigation">
+                  <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href="/web/2ndWeek/home.php">
+                   home
+                 </button>
+                 <a class="mdl-navigation__link" href="#">World.</a>
+                 <a class="mdl-navigation__link" href="#">How</a>
+                 <a class="mdl-navigation__link" href="#">Are</a>
+                 <a class="mdl-navigation__link" href="#">You?</a>
+               </nav>
+             </div>
+           </header>
   <div class="mdl-layout__drawer">
     <span class="mdl-layout__title">Assignments</span>
     <nav class="mdl-navigation">
-
       <a class="mdl-navigation__link" href="../HelloWorld.html">Week 01</a>
       <a class="mdl-navigation__link" href="../2ndWeek/home.php">Week 02</a>
     </nav>
@@ -34,7 +35,7 @@
   <main class="mdl-layout__content" id="main">
     <div>
       <header>
-         <h2 style="text-align: center;"> Welcome to Austin Davis's Home Page </h1>
+         <h2> Welcome to Austin Davis's Home Page </h1>
       </header>
 
       <div>
