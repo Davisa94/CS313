50bddd6ca0d05e344865803017096486e0688838
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-06 16:28:55 -0600

    Added some more styling and fixed some styling errors

--- web/2ndWeek/aboutMe.php
+++ web/2ndWeek/aboutMe.php
@@ -8,33 +8,42 @@
             <!-- This div is for the SBS content -->
             <h3>A Little About Me</h3>
             <div class="grid">
-               <ul class="mdl-list nested gold-gradient">
-                  <li class="mdl-list__item">
-                     <h3>My Gaming Preferences</h3>
-                     <span class="mdl-list__item-primary-content">
-                        I love to play minecraft and terraria, and lots of other games. Mainly games that are
-                        open-world or sandbox or some combination of both
-                     </span>
-                  </li>
-               </ul>
-               <ul class="mdl-list nested bronze-gradient">
-                  <li class="mdl-list__item">
-                     <h3>My interests and Hobbies</h3>
-                     <span class="mdl-list__item-primary-content">
-                        I love to play minecraft and terraria, and lots of other games. Mainly games that are
-                        open-world or sandbox or some combination of both
-                     </span>
-                  </li>
-               </ul>
-               <ul class="mdl-list nested steel">
-                  <li class="mdl-list__item">
-                     <h3>My Career Aspirations</h3>
-                     <span class="mdl-list__item-primary-content">
-                        I love to play minecraft and terraria, and lots of other games. Mainly games that are
-                        open-world or sandbox or some combination of both
-                     </span>
-                  </li>
-               </ul>
+               <div>
+                  <h3>My Gaming Preferences</h3>
+                  <ul class="mdl-list nested gold-gradient">
+                     <li class="mdl-list__item">
+
+                        <span class="mdl-list__item-primary-content">
+                           I love to play minecraft and terraria, and lots of other games. Mainly games that are
+                           open-world or sandbox or some combination of both. I also enjoy the Legend of Zelda
+                           Series of games, my favorties being Wind Waker, OOT, and Majoras Mask
+                        </span>
+                     </li>
+                  </ul>
+               </div>
+               <div>
+                  <h3>My Interests and Hobbies</h3>
+                  <ul class="mdl-list nested bronze-gradient">
+                     <li class="mdl-list__item">
+
+                        <span class="mdl-list__item-primary-content">
+                           I love to play minecraft and terraria, and lots of other games. Mainly games that are
+                           open-world or sandbox or some combination of both
+                        </span>
+                     </li>
+                  </ul>
+               </div>
+               <div>
+                  <h3>My Career Aspirations</h3>
+                  <ul class="mdl-list nested steel-gradient">
+                     <li class="mdl-list__item">
+                        <span class="mdl-list__item-primary-content">
+                           I love to play minecraft and terraria, and lots of other games. Mainly games that are
+                           open-world or sandbox or some combination of both
+                        </span>
+                     </li>
+                  </ul>
+               </div>
             </div>
 
          </main>
