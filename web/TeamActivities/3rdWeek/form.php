56e58f1ec7028273e46463ad5e178c9e1a51a170
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-08 15:50:20 -0600

    Added breaks

--- web/TeamActivities/3rdWeek/form.php
+++ web/TeamActivities/3rdWeek/form.php
@@ -1,9 +1,15 @@
 Name: <input type="text" name="name">
+<br />
 E-mail: <input type="text" name="email">
+<br/>
 Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-
+<br />
 Major:
+<br />
 <input type="radio" name="major" value="CS">Computer Science
+<br />
 <input type="radio" name="major" value="WDD">WDD
+<br />
 <input type="radio" name="major" value="CIT">CIT
+<br />
 <input type="radio" name="major" value="CE">CE
