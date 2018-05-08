9a1875e494a638c603fdedcfe8b13deb61d4e87f
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-08 17:47:38 -0600

    Initial push for ePortfolio

---
+++ web/2ndWeek/portfolio.php
@@ -0,0 +1,31 @@
+<html>
+<?php
+   require 'head.php';
+ ?>
+
+<body>
+
+      <div class="mdl-layout mdl-js-layout">
+<?php
+   require 'nav.php';
+ ?>
+  <main class="mdl-layout__content" id="main">
+    <div>
+      <header>
+         <h2> My Portfolio </h1>
+      </header>
+
+      <div>
+
+      </div>
+   </div>
+  </main>
+</div>
+         <!-- <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+           Assignments
+         </button>
+      </div>
+-->
+
+</body>
+</html>
