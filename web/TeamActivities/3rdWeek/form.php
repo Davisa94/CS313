145425e1c248f660bb0fd60f1d1326f0415813ac
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-08 15:48:14 -0600

    Just getting started

---
+++ web/TeamActivities/3rdWeek/form.php
@@ -0,0 +1,9 @@
+Name: <input type="text" name="name">
+E-mail: <input type="text" name="email">
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+
+Major:
+<input type="radio" name="major" value="CS">Computer Science
+<input type="radio" name="major" value="WDD">WDD
+<input type="radio" name="major" value="CIT">CIT
+<input type="radio" name="major" value="CE">CE
