612c8484e7038ade5eb2ca58a6c5bfeae3f1509c
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-08 16:03:37 -0600

    Fixed an error

--- web/TeamActivities/3rdWeek/form.php
+++ web/TeamActivities/3rdWeek/form.php
@@ -1,5 +1,5 @@
 
-<form action="/action_page.php" method="post">
+<form action="/submit.php" method="post">
    Name: <input type="text" name="name">
    <br />
    E-mail: <input type="text" name="email">
