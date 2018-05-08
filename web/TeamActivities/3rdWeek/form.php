9bc145350a6cd82ad8fa191b77abf39422885e64
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-08 15:52:19 -0600

    Added everything into a form

--- web/TeamActivities/3rdWeek/form.php
+++ web/TeamActivities/3rdWeek/form.php
@@ -1,15 +1,23 @@
-Name: <input type="text" name="name">
-<br />
-E-mail: <input type="text" name="email">
-<br/>
-Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-<br />
-Major:
-<br />
-<input type="radio" name="major" value="CS">Computer Science
-<br />
-<input type="radio" name="major" value="WDD">WDD
-<br />
-<input type="radio" name="major" value="CIT">CIT
-<br />
-<input type="radio" name="major" value="CE">CE
+
+<form onsubmit="submit.php">
+   Name: <input type="text" name="name">
+   <br />
+   E-mail: <input type="text" name="email">
+   <br/>
+   Comment:
+   <br />
+   <textarea name="comment" rows="5" cols="40"></textarea>
+   <br />
+   Major:
+   <br />
+   <input type="radio" name="major" value="CS">Computer Science
+   <br />
+   <input type="radio" name="major" value="WDD">WDD
+   <br />
+   <input type="radio" name="major" value="CIT">CIT
+   <br />
+   <input type="radio" name="major" value="CE">CE
+   <br />
+   <input type="submit" name="submit" value="submit">
+
+</form>
