66b7dbef1c045a6d50f8ad78a5297c32c22bda85
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-09 16:42:46 -0600

    Added more content to Eportfolio

--- web/2ndWeek/portfolio.php
+++ web/2ndWeek/portfolio.php
@@ -1,31 +1,31 @@
 <html>
-<?php
-   require 'head.php';
- ?>
+   <?php require 'head.php'; ?>
 
-<body>
-
+   <body>
       <div class="mdl-layout mdl-js-layout">
-<?php
-   require 'nav.php';
- ?>
-  <main class="mdl-layout__content" id="main">
-    <div>
-      <header>
-         <h2> My Portfolio </h1>
-      </header>
+         <?php require 'nav.php'; ?>
+         <main class="mdl-layout__content" id="other">
+            <!-- This div is for the SBS content -->
+            <h3>A Little More About Me</h3>
+            <div class="grid">
+               <div>
+                  <h3>Academic Accomplishments</h3>
+                  <div class="mdl-list nested gold-gradient">
+                        <span class="content">
+                           Here is a link to a website created for a web
+                           engineering course: <a href=https://davisa94.github.io/public_html/>Assignments</a>
+                        </span>
+                  </div>
+               </div>
+               <div>
+                  <h3>3D Modeling/Animation: Crystal</h3>
+                  <div class="mdl-list nested gold-gradient">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/OMVwMctDZqg?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                  </div>
+               </div>
 
-      <div>
-
+         </main>
       </div>
-   </div>
-  </main>
-</div>
-         <!-- <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-           Assignments
-         </button>
-      </div>
--->
+   </body>
 
-</body>
 </html>
