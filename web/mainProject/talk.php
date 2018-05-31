b8a2dd1803b69aac43933a37445dbaa270f8e6f2
Austin Benitez <austin.i.davis94@gmail.com>
2018-05-31 16:44:52 -0600

    Added more content to the talk page

--- web/mainProject/talk.php
+++ web/mainProject/talk.php
@@ -1,4 +1,5 @@
 <?php
+// TODO: Change this to look up the given dialouge_id
   require 'dbConnect.php';
   $id = htmlspecialchars($_POST['character']);
   $db = get_db();
@@ -11,4 +12,18 @@
   $row = $statement->fetch();
 
   echo $row['body'];
+
+  $query = "select id, body, next_dialouge_id, FROM user_response WHERE character_dialouge_id = :id";
+
+  $statement = $db->prepare($query);
+  $statement->bindValue(":id", $row['id'], PDO::PARAM_INT);
+  $statement->execute();
+  $responses = $statement->fetchAll(PDO::FETCH_ASSOC);
+  echo "<form action=\"talk.php\" method=\"POST\">";
+  foreach $response in $responses{
+    $body = $response['body'];
+    $next_id = $response['next_dialouge_id'];
+    echo "<button type=\"submit\" name=\"$next_id\">$body</span>";
+  };
+  echo "</form>";
 ?>
