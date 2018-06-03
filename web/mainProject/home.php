8471ccd3b0e08da891c29b2c5f5b298601c6ba77
Austin Benitez <austin.i.davis94@gmail.com>
2018-06-02 18:44:14 -0600

    updated the logic of the home page again

--- web/mainProject/home.php
+++ web/mainProject/home.php
@@ -28,26 +28,18 @@
             $dbName = ltrim($dbopts["path"],'/');
             $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//get the character info
-            $query = "SELECT id, name FROM character";
-            $statement = $db->prepare($query);
-            $statement->execute();
-            $characters = $statement->fetchAll(PDO::FETCH_ASSOC);
-//get the first phrase for that character
 
-            foreach ($characters as $character) {
-              //get the id to the first dialouge for that character
+            foreach ($db->query('SELECT id, name FROM "character"') as $row) {
               $query = "SELECT DISTINCT ON(id) id, FROM character_dialouge WHERE id = :id ORDER BY id";
-              $start_id = $character['id'];
+              $start_id = $row['id'];
               $statement = $db->prepare($query);
               $statement->bindValue(":id", $start_id, PDO::PARAM_INT);
               $statement->execute();
               $dialouge = $statement->fetchAll(PDO::FETCH_ASSOC);
-
             		echo "<button class = \"mdl-button mdl-js-button
                 mdl-button--raised mdl-js-ripple-effect mdl-button--accent\"
                 name = \"character\" type=\"submit\" value=\"". $dialouge['id'] . "\" href=\"talk.php\">" .
-                $character['name'] . "</button>";
+                $row['name'] . "</button>";
             }
         ?>
         </form>
