8ec93543bfcdfb9f8c8591025147b0bbd2386332
Austin Benitez <austin.i.davis94@gmail.com>
2018-06-11 22:27:10 -0600

    hopefully fixed dynamic nav bar

--- web/mainProject/sessionButton.php
+++ web/mainProject/sessionButton.php
@@ -1,12 +1,12 @@
 <?php
 $loggedin = $_SESSION['username'];
 $active = "";
-if(!$loggedin)
+if($loggedin)
 {
-   echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/login.php\">Login</a>";
-   echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/signup.php\">Signup</a>";
+   echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/logout.php\">Logout</a>";
 }
 else{
-   echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/logout.php\">Logout</a>";
+  echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/login.php\">Login</a>";
+  echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/signup.php\">Signup</a>";
 }
  ?>
