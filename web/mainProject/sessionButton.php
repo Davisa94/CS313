5aa70745c491b17f250f1f162c959ea9d9a1fafe
Austin Benitez <austin.i.davis94@gmail.com>
2018-06-12 10:27:02 -0600

    Updated the session Buttons for login and sign up to be more dynamic

--- web/mainProject/sessionButton.php
+++ web/mainProject/sessionButton.php
@@ -6,7 +6,7 @@ if($loggedin)
 {
    echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/logout.php\">Logout</a>";
 }
-else{
+else if(strpos($url, 'login') == false){
   echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/login.php\">Login</a>";
   echo "<a class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\" href=\"../mainProject/signup.php\">Signup</a>";
 }
