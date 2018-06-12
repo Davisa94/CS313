61fd09a70c8f4189c88f25e907c2eba741793d2b
Austin Benitez <austin.i.davis94@gmail.com>
2018-06-12 12:56:43 -0600

    added missing variable

--- web/mainProject/events.php
+++ web/mainProject/events.php
@@ -3,13 +3,15 @@ session_start();
 #Characters:
 $NEPHI = 1;
 $LEHI = 2;
-$SARIAH = 3; 
+$SARIAH = 3;
 $LEMUEL = 5;
 $LAMAN = 6;
 $LABAN = 4;
+$event = false;
 require 'getCharacterRelation.php';
 if ($id == $LABAN){
   if($relationships[$LABAN] < -4){
+         $event = true;
          $dialouge_id = 9 //Laban is getting Angry
          $query = "select body, id FROM character_dialogue WHERE id = :id";
          $statement = $db->prepare($query);
