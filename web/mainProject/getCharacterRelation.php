f047e16b23568b923aefef02bc19ba06a8f71932
Austin Benitez <austin.i.davis94@gmail.com>
2018-06-12 13:11:46 -0600

    removed some redundant code

--- web/mainProject/getCharacterRelation.php
+++ web/mainProject/getCharacterRelation.php
@@ -4,7 +4,6 @@
 // TODO:  get user id from db
 // TODO: get users relationship data from DB
 //TODO: Store relationship data in an array sorted by character id
- $db = get_db();
  $query = "select * FROM user_relationship WHERE user_id = :user_id ORDER BY character_id";
  $statement = $db->prepare($query);
  $statement->bindValue(":user_name", $user, PDO::PARAM_STR);
